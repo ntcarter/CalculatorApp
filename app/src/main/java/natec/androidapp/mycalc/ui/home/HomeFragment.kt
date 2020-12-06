bea91dbe21b41c21ef32c7532052eaff0199e411
ntcarter <ntcarter@iastate.edu>
2020-12-06 13:46:05 -0600

    added some button functionality for testing the viewModel and the initial layout for the calculator (no functionality yet)

--- app/src/main/java/natec/androidapp/mycalc/ui/home/HomeFragment.kt
+++ app/src/main/java/natec/androidapp/mycalc/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -13,19 +14,30 @@ import natec.androidapp.mycalc.R
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private var numberAdd = 0
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = view.findViewById(R.id.text_home)
+
+        homeViewModel.liveHomeText.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
-        return root
+
+        initButtons(view)
+        return view
+    }
+
+
+    private fun initButtons(view: View){
+        val buttonAdd = view.findViewById<Button>(R.id.buttonAdd)
+        val buttonSub = view.findViewById<Button>(R.id.buttonSub)
+        val buttonChangeText = view.findViewById<Button>(R.id.setText)
+
+        buttonAdd.setOnClickListener { homeViewModel.addOne() }
+        buttonSub.setOnClickListener { homeViewModel.subOne() }
+        buttonChangeText.setOnClickListener { homeViewModel.changeText() }
     }
 }
--- app/src/main/java/natec/androidapp/mycalc/ui/home/HomeViewModel.kt
+++ app/src/main/java/natec/androidapp/mycalc/ui/home/HomeViewModel.kt
@@ -1,13 +1,34 @@
 package natec.androidapp.mycalc.ui.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+private const val TAG = "HomeViewModel"
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private var numberAdd: Int = 0
+
+    //create a mutableLiveData which can be manipulated from this class
+    private val homeText = MutableLiveData<String>("This is the Home Fragment button pressed: $numberAdd")
+    val liveHomeText: LiveData<String>
+        get() = homeText
+
+    fun addOne(){
+        numberAdd++
+        homeText.value = "This is the Home Fragment button pressed: $numberAdd"
+        Log.d(TAG, ".addOne: $numberAdd")
     }
-    val text: LiveData<String> = _text
+
+    fun changeText(){
+        if(numberAdd % 2 == 0){
+            homeText.value = "Even number so the text is changed"
+        }
+    }
+
+    fun subOne(){
+        numberAdd--
+        homeText.value = "This is the Home Fragment button pressed: $numberAdd"
+    }
 }
